487897f024f372755b578fd4657f678a0793985d
ovidiu1 <ovidiu@ramp-ops.com>
2017-05-05 16:25:21 +0300

    add,edit,delete - tabs icons

--- app/constants/add.item.constant.js
+++ app/constants/add.item.constant.js
@@ -7,22 +7,26 @@
                 date:"04/04/2017",
                 time: "11:02",
                 status: "Done!",
-                color:"red"
+                color:"red",
+                editing: false
+
             },
             {
                 content:"Text 2",
-                category: "Work",
+                category: "Life",
                 date:"04/04/2017",
                 time: "05:02",
-                status: "In Progress!"
-              
+                status: "In Progress!",
+                editing: false
+                
             },
             {
                 content:"Text 3",
                 category:"Work",
                 date:"04/04/2017",
                 time: "03:22",
-                status: "Done!"
+                status: "Done!",
+                editing: false                
             }
         ]
     });
--- app/pages/home/home.controller.js
+++ app/pages/home/home.controller.js
@@ -9,25 +9,17 @@
 
          ho.getData = getData;
          ho.addItem = addItem;
-         ho.status = status;
-
-            ho.obj = new Date();
-            ho.format = $filter('date')(ho.obj, "dd/MM/yyyy");
-            ho.time = $filter('date')(ho.obj, "hh:mm");
-            console.log(ho.data);
-            console.log(ho.time);            
-
+         ho.selectItem = selectItem;
+         ho.removeItem = removeItem;
+         ho.editItem = editItem;
+         ho.doneEdit = doneEdit;
+        
+        ho.obj = new Date();
+        ho.format = $filter('date')(ho.obj, "dd/MM/yyyy");
+        ho.time = $filter('date')(ho.obj, "hh:mm");
+           
         function getData(){
            ho.data = ItemData.Data;
-           console.log(ho.data)
-        }
-
-        function status(){
-            if (ho.data.status == "Done!"){
-                return ho.st = done;
-            }else{
-                return ho.st = progress;
-            }
         }
 
         function addItem(){
@@ -39,9 +31,27 @@
                 time:ho.time
             };
             ho.data.push(newItem);
+        }   
+
+        function selectItem(item){
+            ho.item = item;
+            console.log(ho.item);
         }
+
+        function removeItem(item){
+            var index = ho.data.indexOf(item);
+            ho.data.splice(index, 1);
+        }
+        
+        function editItem(item){
+            item.editing = true;
+            console.log(item.editing);
+        }
+
+        function doneEdit(item){
+            item.editing = true;            
+        }
+
         ho.getData();   
-        ho.test = ho.category;
-        console.log(ho.test);
     }
 })();
